7377c8e48752becf7718e7772a4d4ec241582a7d
Oren Shoham <oshoham@oberlin.edu>
2016-02-22 11:49:20 -0500

    Set up react + webpack + eslint

---
+++ flocabulary_url_shortener/url_shortener/static/url_shortener/js/src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default React.createClass({
+  displayName: 'App',
+  render: function () {
+    return <h1>Hello world!</h1>;
+  }
+});
---
+++ flocabulary_url_shortener/url_shortener/static/url_shortener/js/webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+
+  entry: './src/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+  output: {
+    path: path.resolve('./dist/'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+    extensions: ['', '.js', '.jsx']
+  }
+};
