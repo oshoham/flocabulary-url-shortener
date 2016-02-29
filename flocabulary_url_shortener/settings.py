7760dfd8fd06d9206e3d32f2a4e3bfa5d0db756b
Oren Shoham <oshoham@oberlin.edu>
2016-02-29 13:45:48 -0500

    Use dj-database-url to manage Postgres connection

--- flocabulary_url_shortener/settings.py
+++ flocabulary_url_shortener/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,14 +77,7 @@ WSGI_APPLICATION = 'flocabulary_url_shortener.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urlshortener',
-        'USER': 'urlshortener',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(default='postgres://urlshortener:password@localhost:5432/urlshortener')
 }
 
 
